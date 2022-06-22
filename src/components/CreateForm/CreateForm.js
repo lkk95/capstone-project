49fac396f1be001dc592cff015f8f9c1ddebc39f
Laura Kesel <laura.kesel@gmail.com>
2022-06-22 17:54:24 +0200

    add states and onChange functions

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -12,14 +12,10 @@ export default function CreateForm() {
 					<legend>Choose the category of your recipe:</legend>
 					<input type="radio" name="category" id="breakfast" value="breakfast" />
 					<label htmlFor="breakfast">Breakfast</label>
-					<section>
-						<input type="radio" name="category" id="lunch" value="lunch" />
-						<label htmlFor="lunch">Lunch</label>
-					</section>
-					<section>
-						<input type="radio" name="category" id="dinner" value="dinner" />
-						<label htmlFor="dinner">Dinner</label>
-					</section>
+					<input type="radio" name="category" id="lunch" value="lunch" />
+					<label htmlFor="lunch">Lunch</label>
+					<input type="radio" name="category" id="dinner" value="dinner" />
+					<label htmlFor="dinner">Dinner</label>
 				</fieldset>
 				<input type="submit" value="Add" />
 			</FormContainer>
