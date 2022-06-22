fada6f9dbd6db7c0e278fd4352c057ccac617c8a
Laura Kesel <laura.kesel@gmail.com>
2022-06-22 12:05:13 +0200

    fix more linting errors

--- src/components/MealCard/styled.js
+++ src/components/MealCard/styled.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 const MealCardContainer = styled.article`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
 	align-items: center;
-	height: 8em;
+	justify-content: center;
 	width: 20em;
+	height: 8em;
 	margin: 1em 0 1em 0;
 	border: solid 0.1em black;
 `;
