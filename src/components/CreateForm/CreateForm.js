cad4246cf00ae531c121581c0b0adf1fa42dc713
Laura Kesel <laura.kesel@gmail.com>
2022-06-22 17:56:04 +0200

    display input values on page for test

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -3,12 +3,20 @@ import {useState} from 'react';
 import FormContainer from './styled.js';
 
 export default function CreateForm() {
+	const [name, setName] = useState('');
+	const [category, setCategory] = useState('');
+
 	return (
 		<>
 			<FormContainer>
 				<label htmlFor="title">Recipe Name:</label>
-				<input type="text" name="title" id="title" />
-				<fieldset>
+				<input
+					type="text"
+					name="title"
+					id="title"
+					onChange={event => setName(event.target.value)}
+				/>
+				<fieldset onChange={event => setCategory(event.target.value)}>
 					<legend>Choose the category of your recipe:</legend>
 					<input type="radio" name="category" id="breakfast" value="breakfast" />
 					<label htmlFor="breakfast">Breakfast</label>
@@ -19,6 +27,8 @@ export default function CreateForm() {
 				</fieldset>
 				<input type="submit" value="Add" />
 			</FormContainer>
+			<p>{name}</p>
+			<p>{category}</p>
 		</>
 	);
 }
