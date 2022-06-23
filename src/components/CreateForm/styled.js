bd66781b1d70c1ab217c1e3b2975306f439cbede
Laura Kesel <laura.kesel@gmail.com>
2022-06-23 11:36:52 +0200

    add basic styling

--- src/components/CreateForm/styled.js
+++ src/components/CreateForm/styled.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const FormContainer = styled.form`
 	display: flex;
 	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 1em;
 `;
 
 export default FormContainer;
