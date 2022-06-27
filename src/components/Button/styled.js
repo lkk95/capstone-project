9f2ba0f4636adba132cedaf6a262e7382473302f
Laura Kesel <laura.kesel@gmail.com>
2022-06-27 11:09:10 +0200

    add styling to Form and Button component

---
+++ src/components/Button/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+	width: 6em;
+	height: 2.5em;
+	border: solid 0 black;
+	border-radius: 2em;
+	background-color: #f4d03f;
+	cursor: pointer;
+`;
+
+export default StyledButton;
