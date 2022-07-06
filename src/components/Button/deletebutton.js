fb7f5b34adbdfb49b6e242917deae775d955f7b6
Laura Kesel <laura.kesel@gmail.com>
2022-07-06 10:30:13 +0200

    add routing back to homepage to delete button

--- src/components/Button/deletebutton.js
+++ src/components/Button/deletebutton.js
@@ -1,12 +1,20 @@
+import {useNavigate} from 'react-router-dom';
+
 import useStore from '../../hooks/useStore.js';
 
 import StyledEdit from './styledicon';
 
 export default function DeleteButton({id}) {
 	const deleteMeal = useStore(state => state.deleteMeal);
+	const navigate = useNavigate();
 
 	return (
-		<StyledEdit onClick={() => deleteMeal(id)}>
+		<StyledEdit
+			onClick={() => {
+				deleteMeal(id);
+				navigate('/');
+			}}
+		>
 			<i className="fa-solid fa-trash"></i>
 		</StyledEdit>
 	);
