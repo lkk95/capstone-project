26094af5a5b6a24938706d70db262c7177fbb97d
Laura Kesel <laura.kesel@gmail.com>
2022-07-11 12:45:07 +0200

    remove unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,6 @@ import CreateForm from '../components/CreateForm/index.js';
 import Layout from '../components/Layout/index.js';
 
 export default function HomePage() {
-	//const allMeals = useStore(state => state.allMeals);
-	//{allMeals.map(meal => {
-	//return <MealCard key={meal.id} id={meal.id} />;
-	//})}
 	return (
 		<Layout>
 			<Helmet>
