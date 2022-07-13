bbe24a65703f06e4df4c115b3a9d6b7c2e272ce3
Laura Kesel <laura.kesel@gmail.com>
2022-07-13 11:35:39 +0200

    update 2

--- src/pages/shoppinglist.js
+++ src/pages/shoppinglist.js
@@ -3,13 +3,16 @@ import {Helmet} from 'react-helmet';
 import IngredientList from '../components/Ingredientlist/index.js';
 import Layout from '../components/Layout/index.js';
 
+import StyledHeading from './styledheading.js';
+
 export default function ShoppingList() {
 	return (
 		<Layout>
 			<Helmet>
-				<title key="title">ShoppingList</title>
+				<title key="title">Shopping List</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Helmet>
+			<StyledHeading>Shopping List</StyledHeading>
 			<IngredientList />
 		</Layout>
 	);
---
+++ src/pages/styledheading.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledHeading = styled.h1`
+	background: #e0e0e0;
+`;
+
+export default StyledHeading;
