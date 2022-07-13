160d882b0d6b4d68e4037adc872a0f2e68553ebd
Laura Kesel <laura.kesel@gmail.com>
2022-07-13 11:09:03 +0200

    refactor go back buttons to navigate to plannerpage

--- src/pages/details.js
+++ src/pages/details.js
@@ -2,19 +2,18 @@ import {Helmet} from 'react-helmet';
 
 import Details from '../components/Details/index.js';
 import EditForm from '../components/EditForm/index.js';
-import Layout from '../components/Layout/index.js';
 import useStore from '../hooks/useStore.js';
 
 export default function DetailPage() {
 	const isEditing = useStore(state => state.isEditing);
 
 	return (
-		<Layout>
+		<>
 			<Helmet>
 				<title key="title">Details</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Helmet>
 			{isEditing ? <EditForm /> : <Details />}
-		</Layout>
+		</>
 	);
 }
