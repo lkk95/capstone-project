16bb7a5448de3eb3c16e5427f39b4a45751c19cc
Laura Kesel <laura.kesel@gmail.com>
2022-07-13 11:16:17 +0200

    fix linting errors and add alt text to image

--- src/components/Layout/styledheader.js
+++ src/components/Layout/styledheader.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.header`
 	display: flex;
-	justify-content: center;
 	align-items: center;
+	justify-content: center;
 	width: 100%;
 	height: 8em;
 `;
--- src/components/Layout/stylednav.js
+++ src/components/Layout/stylednav.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 const StyledNav = styled.div`
-	height: 4em;
-	width: 100%;
+	display: flex;
 	position: fixed;
 	bottom: 0;
-	display: flex;
-	justify-content: space-around;
 	align-items: center;
+	justify-content: space-around;
+	width: 100%;
+	height: 4em;
 	background-color: #f5c0a2;
 	color: white;
 `;
