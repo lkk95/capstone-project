fe63d93fd4d2037f346b7416608f6efe22033e11
Laura Kesel <laura.kesel@gmail.com>
2022-07-19 14:20:39 +0200

    delete unused css prop

--- src/components/Details/styledlist.js
+++ src/components/Details/styledlist.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const StyledList = styled.ul`
 	margin: 1em auto;
 	padding: 1em;
-	border: 0 black solid;
 	border-radius: 0.4em;
 	line-height: 150%;
 	text-align: left;
