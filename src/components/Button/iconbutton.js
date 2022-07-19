4dc0ee74e34c01c07a876e17b9a7deddd61e88c0
Laura Kesel <laura.kesel@gmail.com>
2022-07-19 11:29:53 +0200

    fix linting errors

--- src/components/Button/iconbutton.js
+++ src/components/Button/iconbutton.js
@@ -7,8 +7,8 @@ const IconButton = styled.button`
 	font-size: ${props => (props.iconStyle === 'homeicon' ? '2.5rem' : '1.3rem')};
 	cursor: pointer;
 	&:hover {
-		font-size: ${props => (props.iconStyle === 'homeicon' ? '3.5rem' : '2.3rem')};
 		transition: ease 1s;
+		font-size: ${props => (props.iconStyle === 'homeicon' ? '3.5rem' : '2.3rem')};
 	}
 `;
 
