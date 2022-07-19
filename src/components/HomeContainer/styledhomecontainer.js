2ab0db3cfb9e0257af21fd82171baeeac8d88ea9
Laura Kesel <laura.kesel@gmail.com>
2022-07-19 10:41:06 +0200

    fix linting error

--- src/components/HomeContainer/styledhomecontainer.js
+++ src/components/HomeContainer/styledhomecontainer.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const StyledHomeContainer = styled.main`
 	display: flex;
 	flex-direction: column;
-	justify-content: space-between;
 	align-items: center;
+	justify-content: space-between;
 	height: 100vh;
 	margin: 2em 0 0 0;
 `;
