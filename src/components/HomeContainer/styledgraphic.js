a52412ecace129d883c51688c563f3ad2e2da779
Laura Kesel <laura.kesel@gmail.com>
2022-07-19 09:29:34 +0200

    fix image

--- src/components/HomeContainer/styledgraphic.js
+++ src/components/HomeContainer/styledgraphic.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 const StyledGraphic = styled.img`
-	height: 18em;
-	width: 38em;
-
-	@media only screen and (max-width: 600px) {
-		position: absolute;
-		bottom: 0em;
-		left: 3em;
+	position: absolute;
+	bottom: 0em;
+	left: 3em;
+	@media only screen and (min-width: 992px) {
+		height: 18em;
+		width: 38em;
+		left: 40%;
 	}
 `;
 
