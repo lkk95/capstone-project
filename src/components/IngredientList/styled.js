ca594f5a151753eb19b40270a5a0dc392887b565
Laura Kesel <laura.kesel@gmail.com>
2022-07-20 09:07:24 +0200

    fix linting errors

--- src/components/IngredientList/styled.js
+++ src/components/IngredientList/styled.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const StyledList = styled.ul`
-	list-style: none;
 	margin: 2em auto;
 	padding: 0;
 	font-weight: 200;
+	list-style: none;
 	input[type='checkbox'] {
 		margin-right: 1em;
 	}
